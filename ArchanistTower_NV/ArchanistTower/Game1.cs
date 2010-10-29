556db2d2c9bcf1a9eec360f606c241460318c59b
seclark1 <seclark1@15e6596c-cdd7-4eb4-b9a8-5ca0474a6eb8>
2010-10-29 17:20:08 +0000

    New Tile Editor System(Very Early Stages)

---
+++ ArchanistTower_NV/ArchanistTower/Game1.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using TiledLib;
+
+namespace ArchanistTower
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Map map;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            map = Content.Load<Map>("Maps/TestMap");
+        }
+
+  
+        protected override void UnloadContent()
+        {        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            map.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ArchanistTower_NV/ArchanistTower/Globals.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace ArchanistTower
+{
+    public static class Globals
+    {
+        public static SpriteBatch spriteBatch;
+        public static GraphicsDeviceManager graphics;
+        public static ContentManager content;
+        public static ResolutionManager Resolution;
+        public static SpriteFont spriteFont;
+
+        public static GraphicsDevice GraphicsDevice
+        {
+            get { return graphics.GraphicsDevice; }
+        }
+
+        public static void Initialize(Game game)
+        {
+            graphics = new GraphicsDeviceManager(game);
+            content = new ContentManager(game.Services);
+            content.RootDirectory = "Content";
+
+            Resolution = new ResolutionManager(ResolutionManager.Res800x600);
+            SetResolution(Resolution.ObtainResolution(), false);
+        }
+
+        public static void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        public static void SetResolution(Vector2 res, bool fullScreen)
+        {
+            graphics.PreferredBackBufferWidth = (int)res.X;
+            graphics.PreferredBackBufferHeight = (int)res.Y;
+
+            graphics.IsFullScreen = fullScreen;
+
+            graphics.ApplyChanges();
+        }
+
+        public static int ScreenWidth
+        {
+            get { return Resolution.GetWidth(); }
+        }
+
+        public static int ScreenHeight
+        {
+            get { return Resolution.GetHeight(); }
+        }
+
+        public static int ScreenMiddleX
+        {
+            get { return Resolution.GetWidth() / 2; }
+        }
+
+        public static int ScreenMiddleY
+        {
+            get { return Resolution.GetHeight() / 2; }
+        }
+
+    }
+}
