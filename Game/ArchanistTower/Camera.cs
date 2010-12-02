f453079bf5ea1e5bcaf4564330f463c1efb84b17
seclark1 <seclark1@15e6596c-cdd7-4eb4-b9a8-5ca0474a6eb8>
2010-12-02 02:26:34 +0000

    Matt A., if you change "zoom" to be something other than 1 in the camera class you will see what I was talking to you about.

--- Game/ArchanistTower/Camera.cs
+++ Game/ArchanistTower/Camera.cs
@@ -9,36 +9,37 @@ namespace ArchanistTower
     public class Camera
     {
         public Vector2 Position = Vector2.Zero;
+        public const float ZOOM = 1.0f;
 
         public Matrix TransformMatrix
         {
-            get { return Matrix.CreateTranslation(new Vector3(-Position, 0f)); }
+            get { return Matrix.CreateTranslation(new Vector3(-Position, 0f)) * Matrix.CreateScale(ZOOM); }
         }
 
         public void LockToTarget(AnimatedSprite sprite, int screenWidth, int screenHeight)
         {
             Position.X =
                 sprite.Position.X +
-                (sprite.CurrentAnimation.CurrentRect.Width / 2) -
-                (screenWidth / 2);
+                ((sprite.CurrentAnimation.CurrentRect.Width * ZOOM) / 2) -
+                (screenWidth / 2) / ZOOM;
             Position.Y =
                 sprite.Position.Y +
-                (sprite.CurrentAnimation.CurrentRect.Height / 2) -
-                (screenHeight / 2);
+                ((sprite.CurrentAnimation.CurrentRect.Height * ZOOM) / 2) -
+                (screenHeight / 2) / ZOOM;
         }
 
         public void ClampToArea(int width, int height)
         {
             //Restrict where the camera can go. (i.e. not off the map)
-            if (Position.X > width)
-                Position.X = width;
-            if (Position.Y > height)
-                Position.Y = height;
+            if (Position.X > width + (((width * ZOOM) - width) / 2))
+                Position.X = width + (((width * ZOOM) - width) / 2);
+            if (Position.Y > height)// * ZOOM)
+                Position.Y = height;// * ZOOM;
 
             if (Position.X < 0)
                 Position.X = 0;
             if (Position.Y < 0)
                 Position.Y = 0;
-        }
+        }                
     }
 }
