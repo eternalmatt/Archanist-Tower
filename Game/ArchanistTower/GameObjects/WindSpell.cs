33570fa42884c72143b82ddb54a7c846bd705b4d
chung7 <chung7@15e6596c-cdd7-4eb4-b9a8-5ca0474a6eb8>
2010-12-11 02:53:39 +0000

    Updated the same collision logic to windspell collisions. Applies to both player and enemy.

--- Game/ArchanistTower/GameObjects/WindSpell.cs
+++ Game/ArchanistTower/GameObjects/WindSpell.cs
@@ -43,46 +43,97 @@ namespace ArchanistTower.GameObjects
             if (o is Enemy && originatingType == OriginatingType.Player)
             {
                 o.Health -= 20;
-                switch (Direction) // stuns the enemy backwards for 20 units
-                {
-                    case FacingDirection.Left:
-                        o.SpriteAnimation.Position.X -= 20;
-                        o.WorldCollision();
-                        break;
-                    case FacingDirection.Right:
-                        o.SpriteAnimation.Position.X += 20;
-                        o.WorldCollision();
-                        break;
-                    case FacingDirection.Up:
-                        o.SpriteAnimation.Position.Y -= 20;
-                        o.WorldCollision();
-                        break;
-                    case FacingDirection.Down:
-                        o.SpriteAnimation.Position.Y += 20;
-                        o.WorldCollision();
-                        break;
-                }
+                //switch (Direction) // stuns the enemy backwards for 20 units
+                //{
+                //    case FacingDirection.Left:
+                //        o.SpriteAnimation.Position.X -= 20;
+                //        o.WorldCollision();
+                //        break;
+                //    case FacingDirection.Right:
+                //        o.SpriteAnimation.Position.X += 20;
+                //        o.WorldCollision();
+                //        break;
+                //    case FacingDirection.Up:
+                //        o.SpriteAnimation.Position.Y -= 20;
+                //        o.WorldCollision();
+                //        break;
+                //    case FacingDirection.Down:
+                //        o.SpriteAnimation.Position.Y += 20;
+                //        o.WorldCollision();
+                //        break;
+                //}
+                checkClipCollision(this, o);
                 Dead = true;
             }
             else if (o is Player && originatingType == OriginatingType.Enemy)
             {
                 o.Health -= 5;
-                switch (Direction) // stuns the player backwards for 20 units
-                {
-                    case FacingDirection.Left:
-                        o.SpriteAnimation.Position.X -= 20;
-                        break;
-                    case FacingDirection.Right:
-                        o.SpriteAnimation.Position.X += 20;
-                        break;
-                    case FacingDirection.Up:
-                        o.SpriteAnimation.Position.Y -= 20;
-                        break;
-                    case FacingDirection.Down:
-                        o.SpriteAnimation.Position.Y += 20;
-                        break;
-                }
+                //switch (Direction) // stuns the player backwards for 20 units
+                //{
+                //    case FacingDirection.Left:
+                //        o.SpriteAnimation.Position.X -= 20;
+                //        break;
+                //    case FacingDirection.Right:
+                //        o.SpriteAnimation.Position.X += 20;
+                //        break;
+                //    case FacingDirection.Up:
+                //        o.SpriteAnimation.Position.Y -= 20;
+                //        break;
+                //    case FacingDirection.Down:
+                //        o.SpriteAnimation.Position.Y += 20;
+                //        break;
+                //}
+                checkClipCollision(this, o);
                 Dead = true;
+            }
+        }
+
+        private void checkClipCollision(GameObject oHits, GameObject oGotHit)
+        {
+            Rectangle futureRectangle = oGotHit.SpriteAnimation.Bounds;
+
+            switch (oHits.Direction)
+            {
+                case FacingDirection.Left:
+                    futureRectangle.X -= 20;
+                    foreach (Rectangle clip in GameWorld.ClipMap.Values)
+                        if (clip.Intersects(futureRectangle))
+                            {
+                                oGotHit.SpriteAnimation.Position.X = clip.Right + 5;
+                                return;
+                            }
+                    oGotHit.SpriteAnimation.Position.X -= 20;
+                    break;
+                case FacingDirection.Right:
+                    futureRectangle.X += 20;
+                    foreach (Rectangle clip in GameWorld.ClipMap.Values)
+                        if (clip.Intersects(futureRectangle))
+                            {
+                                oGotHit.SpriteAnimation.Position.X = clip.Left - 5 - oGotHit.SpriteAnimation.Bounds.Width;
+                                return;
+                            }
+                    oGotHit.SpriteAnimation.Position.X += 20;
+                    break;
+                case FacingDirection.Up:
+                    futureRectangle.Y -= 20;
+                    foreach (Rectangle clip in GameWorld.ClipMap.Values)
+                        if (clip.Intersects(futureRectangle))
+                            {
+                                oGotHit.SpriteAnimation.Position.Y = clip.Bottom + 5;
+                                return;
+                            }
+                    oGotHit.SpriteAnimation.Position.Y -= 20;
+                    break;
+                case FacingDirection.Down:
+                    futureRectangle.Y += 20;
+                    foreach (Rectangle clip in GameWorld.ClipMap.Values)
+                        if (clip.Intersects(futureRectangle))
+                            {
+                                oGotHit.SpriteAnimation.Position.Y = clip.Top - 5 - oGotHit.SpriteAnimation.Bounds.Height;
+                                return;
+                            }
+                    oGotHit.SpriteAnimation.Position.Y += 20;
+                    break;
             }
         }
     }
